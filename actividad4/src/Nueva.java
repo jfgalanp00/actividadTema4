f4d0358c344e8f022372e53ae0b2e43fcd9f84d0
alumno <juanf.galper@educa.jcyl.es>
2021-04-20 20:08:58 +0200

    segunda clase nueva

---
+++ actividad4/src/Nueva.java
@@ -0,0 +1,11 @@
+
+public class Nueva {
+
+	public static void main(String[] args) {
+		System.out.println(" ------------------------------------------");
+		System.out.println(" HOLA MUNDO");
+		System.out.println(" ADIOS MUNDO");
+		System.out.println(" ------------------------------------------");
+	}
+
+}
